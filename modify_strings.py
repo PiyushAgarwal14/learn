3a6634abfd567345ffd1d5c9b9e03f0b13becc0e
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2025-12-12 22:31:17 +0530

    Create modify_strings.py

---
+++ modify_strings.py
@@ -0,0 +1,28 @@
+a, b, c = "apple", "banana", "carrot"
+print(a, b, c)
+
+# slice
+fruits = ["apple", "banana", "mango"]
+print(fruits[0:2])
+
+print(fruits[1:])
+
+print(a[0:3])
+
+print(c[-5:-1])
+
+print(b.upper())
+print(b.lower())
+
+# strip remove first and last white space
+x = " Christmas "
+print(x)
+print(x.strip().lower())
+
+# replace 
+
+j = "Jack Ryan"
+print(j.replace("R", "J"))
+
+#split 
+print(j.split())
