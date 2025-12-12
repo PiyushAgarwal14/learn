1d203f4ee9d8a5d5847d733df487724284aa3aed
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2025-12-12 21:59:13 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4]
+#print (numbers[0])
+
+if (numbers[2] > numbers[3]):
+    print ("True")
+    
+else:
+    print("False")
+
+# camel case
+myName = "Piyush"
+print(myName)
+
+
+#pascal case
+MyName = "Piyush"
+print(MyName)
+
+
+# snake case
+my_name = "Piyush"
+print(my_name)
