7270fa75981e3809d473b305846dd4f0fceb4ffb
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2025-12-13 23:42:26 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,24 @@
+# list
+
+thislist = ["apple", "banana", "cherry"]
+
+thislist[1] = "Mango"
+
+print(thislist)
+
+
+newlist = ["Suzuki", "Mahindra", "Tata", "Honda", "Ferrari"]
+newlist[1:3] = ["Volvo", "Mercedes Benz"]
+
+print(newlist)
+
+#append
+
+Suzuki = ["Wagon R", "Alto", "Ritz", "Ciaz", "Celerio"]
+
+Suzuki.append("Brezza")
+print("\n")
+print(Suzuki)
+
+Suzuki.insert(0,"Dzire")
+print(Suzuki)
