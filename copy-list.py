f46321d49669a7f2ede72414373b8d0ca7ea4709
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2025-12-14 13:24:50 +0530

    Create copy-list.py

---
+++ copy-list.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "mango", "kiwi", "banana", "pear", "grapes"]   
+
+myList = fruits.copy()
+
+print(myList)
+
+print("\n")
+
+a = list(fruits)
+print(a)
+
+print("\n")
+
+newList = fruits[:]
+print(newList)
